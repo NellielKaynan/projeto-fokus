3b8a368d7894e8b7eb9c231dfe4ee1af27e39b17
NellielKaynan <neokaynan@gmail.com>
2024-05-13 11:44:00 -0300

    Iniciando fokus

--- script.js
+++ script.js
@@ -1,16 +1,66 @@
+//............................Variáveis................................//
+
 const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector( '.app__image');
+const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio ('./sons/luna-rise-part-one.mp3');
 
-focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play()
+    } else  {
+        musica.paused
+    }
+});
+
+focoBt.addEventListener ('click', () => {
+    alterarContexto('foco')
+    focoBt.classList.add('active')
 });
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
+    alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 });
 
-longoBt.addEventListener('click', ()  => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-}); 
+longoBt. addEventListener ('click', () => {
+    alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
+});
+
+function alterarContexto (contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active')
+    })
+
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `./imagens/${contexto}.png`)
+
+    switch (contexto) {
+        case 'foco':
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+
+        case 'descanso-curto': 
+            titulo.innerHTML = `
+            Que tal dar uma respirada?
+                <strong class="app__title-strong">Faça uma pausa curta!</strong> 
+            `
+            break;
+        case 'descanso-longo': 
+            titulo.innerHTML = `
+            Hora de voltar à superfície.
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `
+        default:
+            break;
+    }
+};
