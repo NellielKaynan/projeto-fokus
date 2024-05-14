1fdc02363acfaa851b96ca2209faba91652d7888
NellielKaynan <neokaynan@gmail.com>
2024-05-14 13:10:54 -0300

    Adicionando numeros

--- script.js
+++ script.js
@@ -8,14 +8,20 @@ const banner = document.querySelector( '.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
 const startPauseBt  = document.querySelector('#start-pause');
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
+
+const tempoNaTela = document.querySelector('#timer');
+const pausePlayIcon = document.querySelector('.app__card-primary-butto-icon');
+
+
 const musicaFocoInput = document.querySelector('#alternar-musica');
 
-const audioPlay = new Audio('/sons/play.wav');
-const audioPausa = new Audio('/sons/pause.mp3');
+const audioPlay = new Audio('./sons/play.wav');
+const audioPausa = new Audio('./sons/pause.mp3');
 const audioTempoFinalizado = new Audio('./sons/beep.mp3');
 const musica = new Audio ('./sons/luna-rise-part-one.mp3');
 
-let tempoDecorridoEmSegundos = 5
+let tempoDecorridoEmSegundos = 1500
 let intervaloId = null
 
 musicaFocoInput.addEventListener('change', () => {
@@ -74,26 +80,42 @@ function alterarContexto (contexto) {
 };
 
 const contagemRegressiva = () => {
-    if (tempoDecorridoEmSegundos <= 0) {
-        audioTempoFinalizado.play()
+    if(tempoDecorridoEmSegundos <= 0){
+        //audioTempoFinalizado.play()   // áudio executado quando cronômetro finalizar
         zerar()
-        alert('Tempo Finalizado')
+        alert('Tempo finalizado!')
         return
     }
     tempoDecorridoEmSegundos -= 1
-    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
-};
+    mostrarTempo()
+}
 
-startPauseBt.addEventListener('click', iniciar);
+startPauseBt.addEventListener('click', iniciarOuPausar)
 
-function iniciar() {
-    audioPlay()
-    intervaloId = setInterval(contagemRegressiva, 1000)    
+function iniciarOuPausar() {
+    if(intervaloId){
+        audioPausa.play()   // áudio executado quando cronômetro for pausado
+        zerar()
+        return
+    }
+    audioPlay.play()   // áudio executado quando cronômetro iniciar
+    intervaloId = setInterval(contagemRegressiva, 1000)
+    iniciarOuPausarBt.textContent = 'Pausar'
+    pausePlayIcon.src = ("./imagens/pause.png")
 };
 
 function zerar() {
-    audioPausa()
-    clearInterval(intervaloId)
+    clearInterval(intervaloId) 
+    iniciarOuPausarBt.textContent = 'Começar'
     intervaloId = null
+    pausePlayIcon.src = ("./imagens/play_arrow.png")
 };
 
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-br', {minute: '2-digit', second: '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+};
+
+mostrarTempo();
