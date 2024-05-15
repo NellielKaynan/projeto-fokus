8ed6e7e2ec1e0c5fca270d92bf364dbc579cc423
NellielKaynan <neokaynan@gmail.com>
2024-05-15 09:18:32 -0300

    Projeto Fokus Finalizado

--- script.js
+++ script.js
@@ -33,21 +33,25 @@ musicaFocoInput.addEventListener('change', () => {
 });
 
 focoBt.addEventListener ('click', () => {
+    tempoDecorridoEmSegundos = 1500
     alterarContexto('foco')
     focoBt.classList.add('active')
 });
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300
     alterarContexto('descanso-curto')
     curtoBt.classList.add('active')
 });
 
 longoBt. addEventListener ('click', () => {
+    tempoDecorridoEmSegundos = 900
     alterarContexto('descanso-longo')
     longoBt.classList.add('active')
 });
 
 function alterarContexto (contexto) {
+    mostrarTempo()
     botoes.forEach(function (contexto) {
         contexto.classList.remove('active')
     })
@@ -81,7 +85,7 @@ function alterarContexto (contexto) {
 
 const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0){
-        //audioTempoFinalizado.play()   // áudio executado quando cronômetro finalizar
+        audioTempoFinalizado.play()   // áudio executado quando cronômetro finalizar
         zerar()
         alert('Tempo finalizado!')
         return
